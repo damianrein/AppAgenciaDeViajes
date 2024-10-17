69e1de0cf4eabc5ceeea52818b5e9bd883324488
damianrein <damianrein464@gmail.com>
2024-10-17 19:09:09 -0300

    1.0.0

--- src/main/java/com/AppAgenciaDeViajes/security/SecurityConfig.java
+++ src/main/java/com/AppAgenciaDeViajes/security/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.AppAgenciaDeViajes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
+	@Autowired
+	private AuthenticationProvider authProvider;
+	@Autowired
+	private AuthFilter authFilter;
+	
+	public SecurityConfig(AuthenticationProvider authProvider, AuthFilter authFilter) {
+		this.authProvider = authProvider;
+		this.authFilter = authFilter;
+	}
+
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		return null;
+	}
 }
