d2f9d01aec7333991df418b412c355b558e26585
damianrein <damianrein464@gmail.com>
2024-10-18 22:14:29 -0300

    1.0.0

--- src/main/java/com/AppAgenciaDeViajes/repositories/IClientRepository.java
+++ src/main/java/com/AppAgenciaDeViajes/repositories/IClientRepository.java
@@ -1,9 +1,12 @@
 package com.AppAgenciaDeViajes.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.AppAgenciaDeViajes.entities.Client;
 
 public interface IClientRepository extends JpaRepository<Client, Long>{
 
+	Optional<Client> findByEmail(String username);
 }
--- src/main/java/com/AppAgenciaDeViajes/security/ApplicationConfig.java
+++ src/main/java/com/AppAgenciaDeViajes/security/ApplicationConfig.java
@@ -1,8 +1,49 @@
 package com.AppAgenciaDeViajes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.AppAgenciaDeViajes.repositories.IClientRepository;
 
 @Configuration
 public class ApplicationConfig {
 
+	@Autowired
+	private IClientRepository repo;
+
+	public ApplicationConfig(@Autowired IClientRepository repo) {
+		this.repo = repo;
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		return username-> repo.findByEmail(username).orElseThrow(
+				()-> new UsernameNotFoundException("No se encontro el usuario"));
+	}
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
 }
